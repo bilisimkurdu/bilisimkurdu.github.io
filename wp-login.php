921cc2b9291a0b3d892c7c5dd9c7e2a2cfada191
teknokizilelma <44799846+teknokizilelma@users.noreply.github.com>
2020-04-01 20:33:27 +0300

    ADD POSTS

---
+++ wp-login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="tr">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="tr">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Giriş &lsaquo; Bilişim Kurdu &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://bilisimkurdu.github.io/wp-includes/css/dashicons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://bilisimkurdu.github.io/wp-includes/css/buttons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://bilisimkurdu.github.io/wp-admin/css/forms.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://bilisimkurdu.github.io/wp-admin/css/l10n.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://bilisimkurdu.github.io/wp-admin/css/login.min.css?ver=5.4' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://bilisimkurdu.github.io/wp-content/uploads/2018/05/cropped-indir-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://bilisimkurdu.github.io/wp-content/uploads/2018/05/cropped-indir-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://bilisimkurdu.github.io/wp-content/uploads/2018/05/cropped-indir-180x180.png" />
+<meta name="msapplication-TileImage" content="https://bilisimkurdu.github.io/wp-content/uploads/2018/05/cropped-indir-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-tr-tr">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">WordPress'in desteğiyle</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://bilisimkurdu.github.io/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Kullanıcı adı ya da e-posta adresi</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Parola</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Parolayı göster">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Beni hatırla</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
+									<input type="hidden" name="redirect_to" value="https://bilisimkurdu.github.io/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="https://bilisimkurdu.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://bilisimkurdu.github.io/">
+		&larr; Bilişim Kurdu sitesine geri dön		</a></p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://bilisimkurdu.github.io/privacy-policy/">Privacy Policy</a></div>	</div>
+	<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/bilisimkurdu.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Parola g\u00fcc\u00fc bilinmiyor","short":"\u00c7ok zay\u0131f","bad":"Zay\u0131f","good":"Orta","strong":"G\u00fc\u00e7l\u00fc","mismatch":"Parolalar uyu\u015fmuyor"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.4'></script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/bilisimkurdu\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-includes/js/wp-util.min.js?ver=5.4'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var userProfileL10n = {"warn":"Yeni parolan\u0131z kaydedilemedi.","warnWeak":"Zay\u0131f parola kullanmay\u0131 onayla","show":"G\u00f6ster","hide":"Gizle","cancel":"Vazge\u00e7","ariaShow":"Parolay\u0131 g\u00f6ster","ariaHide":"Parolay\u0131 gizle"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bilisimkurdu.github.io/wp-admin/js/user-profile.min.js?ver=5.4'></script>
+<script src="https://my.hellobar.com/fba11ad874363924c22e5b7b974c28732c728079.js" type="text/javascript" charset="utf-8" async="async"></script>	<div class="clear"></div>
+	</body>
+	</html>
+	
