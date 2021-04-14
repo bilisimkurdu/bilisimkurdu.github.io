f03bdb9cc77859b78ed7259dba47204e7dac2c6d
Bilişim Kurdu <bilisimkurdu@cntrl.cf>
2021-04-14 09:49:42 +0300

    Seo optimize

--- wp-login.php
+++ wp-login.php
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/bilisimkurdu\/wp-admin\/admin-ajax.php"}
 <script type="text/javascript" src="https://bilisimkurdu.github.io/wp-includes/js/wp-util.min.js?ver=5.7" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"3c4e663675"};
+var userProfileL10n = {"user_id":"0","nonce":"85a7492fa4"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
